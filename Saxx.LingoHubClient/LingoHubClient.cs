4a2120c600abefe7e9bd01ad45f8a24fe74b4070
Bernhard Wolkerstorfer <bernhard.wolkerstorfer@erecruiter.net>
2015-11-04 10:08:09 +0100

    added wrapper methods for fetching data with LH error extraction

--- Saxx.LingoHubClient/LingoHubClient.cs
+++ Saxx.LingoHubClient/LingoHubClient.cs
@@ -2,6 +2,7 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Saxx.LingoHubClient
 {
@@ -12,6 +13,43 @@ namespace Saxx.LingoHubClient
             var encodedCredentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", username, password)));
             BaseAddress = new Uri("https://api.lingohub.com/v1/");
             DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", encodedCredentials);
+        }
+
+        /// <summary>
+        /// Fetch the data from the LingoHub API.
+        /// </summary>
+        /// <param name="url">URL</param>
+        /// <returns>HTTP Response</returns>
+        private async Task<HttpResponseMessage> Fetch(string url)
+        {
+            var response = await GetAsync(url);
+            if (response.IsSuccessStatusCode)
+            {
+                return response;
+            }
+
+            var error = await response.Content.ReadAsStringAsync();
+            throw new Exception(string.Format("An error occured during request (HTTP {0}):\n{1}\n------", response.StatusCode, error));
+        }
+
+        /// <summary>
+        /// Fetch the data from the LingoHub API as string.
+        /// </summary>
+        /// <param name="url">URL</param>
+        /// <returns>Data as String</returns>
+        private async Task<string> FetchAsString(string url)
+        {
+            return await Fetch(url).Result.Content.ReadAsStringAsync();
+        }
+
+        /// <summary>
+        /// Fetch the data from the LingoHub API as byte array.
+        /// </summary>
+        /// <param name="url">URL</param>
+        /// <returns>Data as String</returns>
+        private async Task<byte[]> FetchAsByteArray(string url)
+        {
+            return await Fetch(url).Result.Content.ReadAsByteArrayAsync();
         }
     }
 }
